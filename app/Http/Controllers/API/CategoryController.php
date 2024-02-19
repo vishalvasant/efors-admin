e2b538e99c9bd0b48b336254fdbe791571e4346d
vishalvasant <vishalvasant8@gmail.com>
2024-02-19 19:16:14 +0530

    Category Module

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -10,7 +10,7 @@ class CategoryController extends BaseController
 {
     public function index(): JsonResponse
     {
-        $categories = Category::isActive()->get(['id', 'name']);
+        $categories = Category::isActive()->get(['id', 'name','img_url']);
         return $this->sendResponse(CategoryResource::collection($categories), 'Categories fetched.');
     }
 
